9d1201110028096879b5bd42c87e9458f7937dc6
Abdal Hussain <abdalhussain5790@gmail.com>
2025-08-26 13:10:19 +0500

    update the codebase

--- server.js
+++ server.js
@@ -2,36 +2,32 @@ import express from "express";
 import cors from 'cors';
 const app = express();
 
-// const connectDB = require('./config/db'); // Using commonJS : old way
 import connectDB from './config/db.js'; // modern way
 import router from "./routes/UserRouters.js";
 import { authRoute } from "./controllers/UserControllers2.js";
 import { authmiddleware } from "./routes/authMiddleware.js";
-// import authroute from "./routes/authRoute.js";
 
 //Middleware
-app.use(cors())
+app.use(cors())//Cross Origin Resource Sharing
 app.use(express.json()) //Send Data to DB
-app.use("/app",router)
-app.use("/auth",authRoute)
+app.use("/app",router)// User Router
+app.use("/auth",authRoute)// Auth Router
+
+// Auth Middleware Route
 app.get("/authmiddleware",authmiddleware,(req,res)=>{
     console.log("User ID: ",req.user.userId);
     console.log("User Email: ",req.user.email);
     console.log("User Username: ",req.user.userName);
     console.log("access authorized");
-
 })
 
-//Defining route
-// app.get('/',(req,res)=>{
-//     res.send("Welcome to Backend Development");
-// });
+//Base Route
+app.get('/',(req,res)=>{
+    res.send("Server is running...");
+});
 
 // Creating server on port 3000
 app.listen(3002,()=>{
     connectDB();
     console.log("Server start at port: http://localhost:3002")
 })
-
-//  Base URL              Middle URL   EndPoint
-// http://localhost:3002 /app/users   /getAllUser
